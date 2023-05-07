3773a570102fe32c95263d69a1dddfeea3d2becb
Viks0777 <132847356+Viks0777@users.noreply.github.com>
2023-05-08 01:18:49 +0600

    Add files via upload

---
+++ const.js
@@ -0,0 +1,18 @@
+const commads = `
+/start - Перезапустить бота
+/help - Помощь
+`
+const text1 =`
+Тест для проверки обработчика и <a href="https://yotube.com/>Ссылка</a>"
+`
+const text2 =`
+Тест для проверки обработчика и <a href="https://yotube.com/>Ссылка</a>"
+`
+const text3 =`
+Тест для проверки обработчика и <a href="https://yotube.com/>Ссылка</a>"
+`
+
+module.exports.commads = commads
+module.exports.text1 =text1
+module.exports.text2 =text2
+module.exports.text3 =text3
---
+++ index.js
@@ -0,0 +1,56 @@
+const {
+  Telegraf, Markup
+ } = require('telegraf')
+const { button } = require('telegraf/typings/markup')
+ require('dotenv').config()
+ const text = require('./const')
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Привет ${ctx.message.form.first_name ? ctx.message.form.first_name :'))
+bot.help((ctx) => ctx.reply(text.commads))
+
+bot.command('course', async (ctx) => {
+    try{
+   await ctx.replyWithHTML('<b>Курсы</b>', Markup.inlineKeyboard(
+        [
+            [Markup,button.callback('Редакторы', 'btn_1'),Markup,button.callback('Обзоры', 'btn_2')],
+            [Markup,button.callback('JS', 'btn_3'),Markup,button.callback('HTML', 'btn_4')]
+        ]
+    ))
+    } catch(e) {
+        console.error(e)
+    }
+})
+
+function addActionBot(name, src,text ){
+    bot.action(name, async (ctx) => {
+        try{
+        await ctx.answerCbQuery()
+        if(src !== false){
+            await ctx.replyWithPhoto({
+                source: src
+            })
+        }
+        await ctx.replyWithHTML(text, {
+        disable_web_page_preview: true
+    })
+         } 
+         
+         catch(e) {
+                console.error(e)
+        }
+    })
+}
+addActionBot('btn_1', './img/1.jpg', text.text1)
+addActionBot('btn_2', './img/2.jpg', text.text2)
+addActionBot('btn_3', false, text.text3)
+
+
+
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
